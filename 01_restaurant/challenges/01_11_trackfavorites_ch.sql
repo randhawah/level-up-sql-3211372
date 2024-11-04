5b68beeb40ac93d70a647bac88acbe78af481f6e
Harman Randhawa <54494701+randhawah@users.noreply.github.com>
2024-11-04 23:13:47 +0000

    Complete Resturant Challange 11

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,14 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+SELECT * 
+FROM Customers 
+WHERE FirstName = 'Loretta' and LastName = 'Hundey';
+
+UPDATE Customers 
+  SET FavoriteDish = (SELECT DishID FROM Dishes WHERE Name = 'Quinoa Salmon Salad') 
+  WHERE FirstName = 'Loretta' and LastName = 'Hundey';
+
+SELECT Customers.FirstName, Customers.LastName, Customers.FavoriteDish, Dishes.Name 
+FROM Customers 
+JOIN Dishes ON Customers.FavoriteDish = Dishes.DishID;
