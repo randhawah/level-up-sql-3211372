080c71006c70d504496b4634e3acb782ec608cae
Harman Randhawa <54494701+randhawah@users.noreply.github.com>
2024-11-04 22:16:43 +0000

    Complete Resturant challange 2

--- 01_restaurant/challenges/01_02_anniversary_ch.sql
+++ 01_restaurant/challenges/01_02_anniversary_ch.sql
@@ -1,9 +1,11 @@
 -- Create a table in the database to store customer
 -- responses to our anniversary invitation.
-select * from Customers;
-select * from Reservations;
+
 -- Associate a customer's ID number with the number of people
 -- they plan to bring in their party.
 
 -- Hint: SQLite offers the INTEGER and REAL datatypes
-select CustomerID, PartySize from Reservations;
+CREATE TABLE AnniversaryAttendees (
+  CustomerID INTEGER,
+  PartySize INTEGER
+);
