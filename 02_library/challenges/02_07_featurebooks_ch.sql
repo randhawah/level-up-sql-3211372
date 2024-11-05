74ed41bb8fdb74507eb51fedd7cb7920b12a7e72
Harman Randhawa <54494701+randhawah@users.noreply.github.com>
2024-11-05 21:02:17 +0000

    Complete Library challange 7

--- 02_library/challenges/02_07_featurebooks_ch.sql
+++ 02_library/challenges/02_07_featurebooks_ch.sql
@@ -2,3 +2,10 @@
 
 -- Make a pick list of books published from 1890-1899 
 -- which are not currently checked out.
+
+Select Title, Author, Published
+From Books
+Where Published BETWEEN 1890 and 1899
+AND BookID not in ( Select BookID 
+                    From Loans
+                    Where ReturnedDate is NULL);
