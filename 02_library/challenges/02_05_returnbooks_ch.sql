e21aaec319f86ae57d45141d3d4b4714ecddea54
Harman Randhawa <54494701+randhawah@users.noreply.github.com>
2024-11-05 20:48:38 +0000

    Complete Library Challange 5

--- 02_library/challenges/02_05_returnbooks_ch.sql
+++ 02_library/challenges/02_05_returnbooks_ch.sql
@@ -4,3 +4,23 @@
 -- Book 1: 6435968624
 -- Book 2: 5677520613
 -- Book 3: 8730298424
+
+UPDATE Loans
+Set ReturnedDate = '2022-07-05'
+Where BookID = (Select BookID From Books Where Barcode = '6435968624')
+                AND ReturnedDate IS NULL;
+
+UPDATE Loans
+Set ReturnedDate = '2022-07-05'
+Where BookID = (Select BookID From Books Where Barcode = '5677520613')
+                AND ReturnedDate IS NULL;
+
+UPDATE Loans
+Set ReturnedDate = '2022-07-05'
+Where BookID = (Select BookID From Books Where Barcode = '8730298424')
+                AND ReturnedDate IS NULL;
+
+
+SELECT Books.Barcode from Books
+Join Loans ON Books.BookID = Loans.BookID
+where Loans.ReturnedDate = '2022-07-05';
