59bd8cf1605946214f54c1e61685a5a2ec5ada22
Harman Randhawa <54494701+randhawah@users.noreply.github.com>
2024-11-05 20:42:06 +0000

    Complete Lobrary Challange 4

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,10 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+
+SELECT Loans.DueDate, Books.Title, Patrons.FirstName, Patrons.Email
+From Loans
+Join Books ON Loans.BookID = Books.BookID
+Join Patrons ON Loans.PatronID = Patrons.PatronID
+Where Loans.DueDate = '2022-07-13'
+AND Loans.ReturnedDate is NULL;
