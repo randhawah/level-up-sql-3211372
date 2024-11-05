ef30694d2b9852da67e97e1b44f1a554a9ded81f
Harman Randhawa <54494701+randhawah@users.noreply.github.com>
2024-11-05 20:38:36 +0000

    Complete Library Challange 3

--- 02_library/challenges/02_03_checkout_ch.sql
+++ 02_library/challenges/02_03_checkout_ch.sql
@@ -3,3 +3,31 @@
 -- Book 2: Great Expectations, 4043822646
 -- The checkout date is August 25, 2022 and the 
 -- due date is September 8, 2022.
+
+SELECT BookID from Books
+where Title = 'The Picture of Dorian Gray' and Barcode =2855934983;
+
+
+SELECT BookID, Title from Books
+where Title = 'Great Expectations' and Barcode =4043822646;
+
+SELECT PatronID from patrons
+where email = 'jvaan@wisdompets.com';
+
+INSERT INTO Loans(BookID, PatronID, LoanDate, DueDate)
+Values(
+    (SELECT BookID from Books
+    where Title = 'The Picture of Dorian Gray' and Barcode =2855934983), 
+    (SELECT PatronID from patrons
+    where email = 'jvaan@wisdompets.com'),
+    '2022-08-25', '2022-09-08'
+    ),
+    (
+    (SELECT BookID from Books
+    where Title = 'Great Expectations' and Barcode =4043822646), 
+    (SELECT PatronID from patrons
+    where email = 'jvaan@wisdompets.com'),
+    '2022-08-25', '2022-09-08'
+    );
+
+Select * from Loans order by LoanID DESC LIMIT 2;
