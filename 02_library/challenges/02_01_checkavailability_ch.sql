0eb36faee74937542565f8233352dd223ac921e9
Harman Randhawa <54494701+randhawah@users.noreply.github.com>
2024-11-05 20:22:13 +0000

    Complete Library challange 1

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,24 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+select Count(*)
+from Books 
+where Title = 'Dracula';
+
+select Count(*)
+from Loans 
+Join Books
+ON Loans.BookID = Books.BookID
+where Books.Title = 'Dracula'
+and Loans.ReturnedDate is null;
+
+select
+(select Count(*)
+from Books 
+where Title = 'Dracula')
+-
+(select Count(*)
+from Loans 
+Join Books
+ON Loans.BookID = Books.BookID
+where Books.Title = 'Dracula'
+and Loans.ReturnedDate is null) as 'Books Available to Loan';
