5426830dd57783a2913f13e7d9d80f131c5d6612
Harman Randhawa <54494701+randhawah@users.noreply.github.com>
2024-11-05 20:54:51 +0000

    Complete Library Challange 6

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,13 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+
+Select  Count(Loans.LoanID) as LoansCount, 
+        Loans.PatronID, 
+        Patrons.FirstName,
+        Patrons.LastName,
+        Patrons.Email
+From Loans
+Join Patrons ON Loans.PatronID = Patrons.PatronID
+Group By Loans.PatronID
+ORDER BY LoansCount
+LIMIT 10;
